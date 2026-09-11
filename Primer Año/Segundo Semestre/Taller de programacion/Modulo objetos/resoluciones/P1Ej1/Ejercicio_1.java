255e957566b8046f7bf0bbd9c2edc8b2e20b2f95
Thiago <thgiles06@gmail.com>
2026-09-11 20:24:50 -0300

    p1ej1

--- Primer Año/Segundo Semestre/Taller de programacion/Modulo objetos/resoluciones/P1Ej1/Ejercicio_1.java
+++ Primer Año/Segundo Semestre/Taller de programacion/Modulo objetos/resoluciones/P1Ej1/Ejercicio_1.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package P1Ej1;
+import PaqueteLectura.GeneradorAleatorio;
 
-/**
- *
- * @author Thiago
- */
 public class Ejercicio_1 {
-    
+    public static void main(String[] args) {
+        // DECLARACION DE VARIABLES
+        
+        int [] vector = new int [10];
+        GeneradorAleatorio.iniciar();
+        int random = GeneradorAleatorio.generarInt(12);
+        int i = 0;
+        
+        
+        // RESOLUCION
+        
+        while(random != 11){ 
+            vector[i] = random;
+            System.out.println("I es: " + (i+1));
+            i++;
+            random = GeneradorAleatorio.generarInt(12);
+        }
+        
+        // IMPRIMIR POR 2 LOS NODOS DEL VECTOR
+        
+        for(i=0; i<10; i++) {
+            System.out.println(vector[i] * 2);
+        }
+        
+    }
 }
