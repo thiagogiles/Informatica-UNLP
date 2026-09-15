1d8fa9e905f685a87ad7b21aebbb12a98f8806c4
Thiago <thgiles06@gmail.com>
2026-09-15 09:39:12 -0300

    ej3 while

--- Primer Año/Segundo Semestre/Taller de programacion/Modulo objetos/resoluciones/P2Ej3/Ejercicio_3.java
+++ Primer Año/Segundo Semestre/Taller de programacion/Modulo objetos/resoluciones/P2Ej3/Ejercicio_3.java
@@ -10,31 +10,40 @@ import PaqueteLectura.Lector;
 public class Ejercicio_3 {
     public static void main(String[] args) {
         String nombre = Lector.leerString();
+        int cant = 0;
+        int j,i=0; 
         Persona [] [] matriz = new Persona [5] [8];
-        boolean fin = false;
-            for(int i=0; i< 5 && !fin; i++){
-                for(int j=0; j< 8; j++){
-                      if(nombre.equals("ZZZ")){
-                          fin = true;
-                }
-                      else{
+        while((!nombre.equals("ZZZ")) && (cant<40)){
+            j=0;
+            while((!nombre.equals("ZZZ")) && (j<8) && (cant<40) ){
                           int dni= Lector.leerInt();
                           int edad= Lector.leerInt();
                           matriz[i][j] = new Persona(nombre,dni,edad);
-                          nombre = Lector.leerString();
+                          cant++;
+                          j++;
+                          if(cant<40){
+                               nombre = Lector.leerString();
+                          }
+                         
+          }
+           i++;
                       }
-            }
+        j=0; i=0;
+        int cont=0;
+        while(cont<cant){
+            System.out.println("Dia " + (i+1) + " turno   " + (j+1) + matriz[i][j].getNombre());
+            cont++;
+            j++;
+                if(j==8);{
+                  j=0;
+                  i++;
         }
-       //
-       
-       for(int i=0; i<5; i++){
-           for(int j=0; j<8; j++){
-               if(matriz[i][j] != null){
-                      System.out.println("El nombre de la persona a entrevistar es: " + matriz[i][j].getNombre() + " en el dia " + (i+1) + " y turno " + (j+1));
-
-               }
-           }
-       }
-     
+        }
     }
 }
+
+
+            
+     
+    
+
